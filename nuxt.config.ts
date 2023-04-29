ec1909899f59601c83ca1acde285904271104b52
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-04-29 12:18:55 +0800

    learned rendering, ssr, csr, hybrid

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,4 +20,19 @@ export default defineNuxtConfig({
             autoprefixer: {},
         },
     },
+    routeRules: {
+        // // Static page generated on-demand, revalidates in background
+        // '/blog/**': { swr: true },
+        // // Static page generated on-demand once
+        // '/articles/**': { static: true },
+        // // Set custom headers matching paths
+        // '/_nuxt/**': { headers: { 'cache-control': 's-maxage=0' } },
+        // // Render these routes with SPA
+        // '/admin/**': { ssr: false },
+        // // Add cors headers
+        // '/api/v1/**': { cors: true },
+        // // Add redirect headers
+        // '/old-page': { redirect: '/new-page' },
+        // '/old-page2': { redirect: { to: '/new-page', statusCode: 302 } }
+    }
 })
