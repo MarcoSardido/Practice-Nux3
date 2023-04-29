fb596375a8377b1b85c9e6ead2a60ae5cc0cfc88
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-04-29 11:30:58 +0800

    learned server, api and routes for backend logics

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,10 @@
 <script setup>
-// definePageMeta({
-//     middleware: 'auth'
-// })
-
-// const { sayHello } = useUtils()
-// const { $sayHello } = useNuxtApp()
-
-// useState Nuxt3 - State management
-// const counter = useCounter(100)
-
-// Pinia State management
-import { useCounterStore } from '~/stores/myStore'
-const store = useCounterStore()
-console.log(store)
+const res = await $fetch('/api/hello')
+console.log(res)
 </script>
 <template>
     <main>
         <h1>Landing page</h1>
-        <!-- <ContentDoc /> -->
-        <h1>Pinia State management</h1>
-        <h3>Count: {{ store.count }}</h3>
-        <h3>Getters: {{ store.doubleCount }}</h3>
-        <button @click="store.increment">+</button>
-        <button @click="store.$reset">Reset Pinia</button>
-
-        <hr>
-        <div>
-            <h1>useStore State management</h1>
-            <Counter />
-        </div>
     </main>
 </template>
 
